61d3e8a41afdb7f2e577e89420c008ffa6c0fa94
RicardoGayle <rgayle@mit.edu>
2021-04-21 01:50:15 -0400

    implemented AugmentedCatalog -- not sure if the tradeoff for >SFB for <RFC is ideal

--- AugmentedCatalog.h
+++ AugmentedCatalog.h
@@ -1,5 +1,4 @@
 #include "AugmentedRecord.h"
-#include "Catalog.h"
 
 #include <list>
 
@@ -8,13 +7,38 @@ using namespace std;
 template <typename T>
 
 /**
- * NOT SURE WHAT TO MAKE OF THIS CLASS IF EVEN NECESSARY
- * 
- * paper says on page 140, "The structure for A_v is more complex.
- *  It can be described as a doubly-linked list of records containing
- *  cross-reference to the records in C_v and with additional information
- *  stored in nodes that are bridges"
+ * @brief Augmented catalogs as described in paper (refered to as A_v).
+ *        Stores the augmented records of the data structure
  */
-class AugmentedCatalog : public Catalog {
+class AugmentedCatalog {
+    /**
+     * AF(listOfRecords) = the catalog which holds the augmented records in the order
+     *          specified in listOfRecords
+     */
+    private:
+        set<AugmentedRecord<T>> setOfRecords;
+        list<AugmentedRecord<T>> listOfRecords;
+    public:
+        Catalog(set<AugmentedRecord<T>> records) {
+            this->setOfRecords = records;
+        };
 
+        Catalog(list<AugmentedRecord<T>> records) {
+            this->listOfRecords = records;
+        }
+
+        /**
+         * @brief Searches for a record in the catalog by key
+         * @param key 
+         * @return Record<T> the record if found, and nullptr if not
+         */
+        AugmentedRecord<T> search(int key) {
+            for (AugmentedRecord<T> record : this->listOfRecords) {
+                // CHANGE THIS TO SETOFRECORDS IF WE USE THAT REP
+                if (record.getKey() == key) {
+                    return record;
+                }
+            }
+            return nullptr;
+        };
 };
