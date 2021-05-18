f58b051d320fa520e41990f40e2572d912fee8ce
RicardoGayle <rgayle@mit.edu>
2021-05-18 13:11:36 -0400

    Working HiveVisualization Class -- will implement parsing next

--- HiveGraphVisualization.py
+++ HiveGraphVisualization.py
@@ -4,28 +4,36 @@ import matplotlib.animation as animation
 from numpy.core.fromnumeric import repeat
 # from matplotlib import pylab as pl
 
-COLORS = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1)])
-FIG, AX = pl.subplots()
+class HiveVisualization:
+    def __init__(self) -> None:
+        self.ORIGINAL_COLOR = np.array([0, 0, 0, 1])
+        self.COLORS = np.array([(1, 0, 0, 1), (0, 1, 0, 1), (0, 0, 1, 1), (1, 1, 0, 1), (0, 1, 1, 1), (1, 0, 1, 1)])
+        self.index = 0
+        self.FIG, self.AX = pl.subplots()
 
-def plot_lines(lines):
-    lc = mc.LineCollection(lines, colors=COLORS, linewidths=2)
-    AX.add_collection(lc)
-    AX.autoscale()
-    AX.margins(0.1)
-    graph_plots = AX.plot()
-    return graph_plots
+    def plot_lines(self, lines):
+        lc = mc.LineCollection(lines, colors= self.ORIGINAL_COLOR, linewidths=2)
+        self.AX.add_collection(lc)
+        self.AX.autoscale()
+        self.AX.margins(0.1)
+        graph_plots = self.AX.plot()
+        return graph_plots
 
-def update_graph(new_line):
-    x1, y1 = new_line[0]
-    x2, y2 = new_line[1]
-    pl.plot((x1, x2), (y1, y2))
+    def update_graph(self, new_set):
+        color = self.COLORS[self.index % len(self.COLORS)]
+        self.index += 1
+        for new_line in new_set:
+            x1, y1 = new_line[0]
+            x2, y2 = new_line[1]
+            pl.plot((x1, x2), (y1, y2), color = color)
 
-def create_plot(lines, new_lines):
-    graph = plot_lines(lines)
-    ani = animation.FuncAnimation(FIG, update_graph, new_lines, interval=1000, blit=False, repeat= False)
-    pl.show()
+    def make_graphs(self, lines, new_lines):
+        graph = self.plot_lines(lines)
+        ani = animation.FuncAnimation(self.FIG, self.update_graph, new_lines, interval=1000, blit=False, repeat= False)
+        pl.show()
 
 lines = [[(0, 1), (1, 1)], [(2, 3), (3, 3)], [(1, 2), (1, 3)]]
-new_lines = [[(2, 1), (2, 2)], [(3, 1), (1, 1)], [(6, 3), (7, 2)]]
-
-create_plot(lines, new_lines)
+new_lines = [([(2, 1), (2, 2)], [(3, 1), (1, 1)], [(6, 3), (7, 2)]),
+            ([(3, 5), (1, 2)], [(1, 4), (2, 6)])]
+hv = HiveVisualization()
+hv.make_graphs(lines, new_lines)
